48de148dba5c2f5e0f48527aa60f0306436c213f
Ngoc Tran <ngoc.q.tran@sjsu.edu>
2021-10-14 18:37:04 -0700

    fix the error

--- calculator.py
+++ calculator.py
@@ -5,12 +5,9 @@ def calculator(number1, number2, operator):
 
 	Parameter
 	--------
-	n1 : float
-	     hold the 1st operation
-	n2 : float
-	     hold the 2nd operation
-	operator : string
-	     hold the symbol of operator
+	n1(float): hold the 1st operation
+	n2(float): hold the 2nd operation
+	operator (string): hold the symbol of operator
 
 	parse two number to float
         return false if the operator is not existence
@@ -45,16 +42,10 @@ def user_input():
 
 	parameter
 	--------
-	prompt : string
-	       get input from user
-	text : string
-	       hold string from split
-	n1 : string
-	       contain 1st number
-	n2 : string
-	       contain 2nd number
-	operator : string
-	       contain 1
+	text(string): hold string from split
+	n1(string): contain 1st number
+	n2(string): contain 2nd number
+	operator(string): contain 1
 
 	get input from user
 	split the input by a delimter that is space
