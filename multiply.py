b21189dfcbc29453303f3960a06f5ae8313cbc53
Ngoc Tran <ngoc.q.tran@sjsu.edu>
2021-10-14 20:27:57 -0700

    fix line 9

--- multiply.py
+++ multiply.py
@@ -3,10 +3,10 @@ def multiply_list(list):
     product = 1
 
     for x in list:
-        if type(x)==int or type(x)==float:
-            product = product * x
+        if type(x) == int or type(x) == float:
+            product *= x
         else:
-	    print("invalid")
+	    print("invalid value!")
             return False
     return product
 
