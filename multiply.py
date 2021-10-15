292f1f86664052685a9e25959e849b9b1e7be144
Ngoc Tran <ngoc.q.tran@sjsu.edu>
2021-10-14 20:53:42 -0700

    fix the error again

--- multiply.py
+++ multiply.py
@@ -1,26 +1,29 @@
 def multiply_list(list):
-	"""
 
-	Multiply each element from list
+"""
 
-	parameter
-	----------
-	number: string
-		hold input from user's prompt
-	list: array
-		list conatin input from user
-	result: int
-		get result from whole list
-	if each element in list is valid, then multiply
-	else return false
+Multiply each element from List
 
-	"""
+Parameter
+--------
+number : string
+      hold input from user's prompt
+list : array of integer
+      list contain input from user
+product : int
+      get result from multiplying whole list
+
+if each element in list is valid, then multiply
+else return False
+
+"""
+
 	product = 1
-	for x in list:
-        	if type(x) == int or type(x) == float:
-            		product *= x
-        	else:
-	    		print('invalid value!')
-            		return False
-		return product
 
+	for n in list:
+		if type(n) == int or type(n) == float:
+			product *= n
+		else:
+			print("Invalid value!!!")
+			return False
+	return product
