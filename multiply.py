a810a197146d236adb52d744e2f772c2dba09a8e
Ngoc Tran <ngoc.q.tran@sjsu.edu>
2021-10-14 20:39:36 -0700

    add docstring

--- multiply.py
+++ multiply.py
@@ -1,12 +1,27 @@
 def multiply_list(list):
+	"""
 
+	Multiply each element from list
+
+	parameter
+	----------
+	number: string
+		hold input from user's prompt
+	list: array
+		list conatin input from user
+	result: int
+		get result from whole list
+	if each element in list is valid, then multiply
+	else return false
+
+	"""
     product = 1
 
     for x in list:
         if type(x) == int or type(x) == float:
             product *= x
         else:
-	    print("invalid value!")
+	    print('invalid value!')
             return False
     return product
 
