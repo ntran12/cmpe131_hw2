dd2113bba998e45c104795a9b0c80ca3fbdcb8cf
Ngoc Tran <ngoc.q.tran@sjsu.edu>
2021-10-14 19:53:42 -0700

    add comment

--- multiply.py
+++ multiply.py
@@ -1,4 +1,23 @@
 def multiply_list(list):
+
+"""
+	Multiply each elements from list
+
+	Parameter
+	---------
+	number: string:
+		hold input from user's prompt
+	list: array:
+		list contain input from user
+	result: int
+		get result from multiply whole list
+
+	if each element in list í valid, put them in list and multiply
+	else, return false
+
+
+
+"""
     product = 1
 
     for x in list:
@@ -6,6 +25,7 @@ def multiply_list(list):
         if type(x)==int or type(x)==float:
             product = product * x
         else:
+	    print("invalid")
             return False
 
     return product
