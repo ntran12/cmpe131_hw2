c49bcf989e29dbc6069677172c48315330127278
Ngoc Tran <ngoc.q.tran@sjsu.edu>
2021-10-14 20:17:38 -0700

    fix space

--- multiply.py
+++ multiply.py
@@ -3,13 +3,10 @@ def multiply_list(list):
     product = 1
 
     for x in list:
-
         if type(x)==int or type(x)==float:
             product = product * x
         else:
 	    print("invalid")
             return False
-
     return product
 
-print(multiply_list([1,2,3,4,5]))
